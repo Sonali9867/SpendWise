056bf036f9e22f36225934bd4c786e599f7e6178
Sonali9678 <sonalikannojiya.221it065@nitk.edu.in>
2025-07-21 00:23:40 +0530

    Update: Fix budget param handling and component logic

--- app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
+++ app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
@@ -1,41 +1,4 @@
-// import React from 'react'
-// import { Expenses } from '../../../../../utils/schema'
-// import { toast } from 'sonner';
 
-// function ExpenseListTable({expensesList}) {
-//     const deleteExpense=async(expense)=>{
-//         const result= await db.delete(Expenses)
-//         .where(eq(Expenses.id,expense.id))
-//         .returning();
-//         if(result){
-//             toast('Expense Deleted!')
-//         }
-//     }
-//   return (
-//     <div className='mt-3'>
-//       <h2 className='font-bold text-lg'>Latest Expenses</h2>
-//       <div className='grid grid-cols-4 bg-slate-200 p-2 mt-3'>
-//         <h2 className='font-semibold'>Name</h2>
-//         <h2 className='font-semibold'>Amount</h2>
-//         <h2 className='font-semibold'>Date</h2>
-//         <h2 className='font-semibold'>Action</h2>
-//       </div>
-//       {expensesList.map((expenses,index)=>(
-//         <div className='grid grid-cols-4 bg-slate-50 p-2'>
-//         <h2>{expenses.name}</h2>
-//         <h2>{expenses.amount}</h2>
-//         <h2>{expenses.createdAt}</h2>
-//         <h2><Trash className='text-red-600 cursor-pointer'
-//            onClick={()=>deleteExpense(expenses)}
-//         /></h2>
-//       </div>
-//       )
-//     )}
-//     </div>
-//   )
-// }
-
-// export default ExpenseListTable
 
 "use client";
 import React from "react";
--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./utils/schema.jsx",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_HuWCiwmr0p5A@ep-blue-cell-a8z24zmr-pooler.eastus2.azure.neon.tech/SpendWise?sslmode=require&channel_binding=require",
+    url: NEXT_PUBLIC_DATABASE_URL,
   }
 });
